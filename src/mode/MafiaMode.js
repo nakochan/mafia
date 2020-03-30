57851ecf3d6d3350766dfcaf95eeac5383f8ff6b
Unyeong Baek <eunnamu094@gmail.com>
2020-03-30 21:06:50 +0900

    fix bug

--- src/mode/MafiaMode.js
+++ src/mode/MafiaMode.js
@@ -278,6 +278,8 @@ module.exports = class RescueMode {
                 case STATE_NIGHT:
                     if (self.game.job === JobType.MAFIA || (self.game.job === JobType.SPY && self.game.touch) || (self.game.job === JobType.BITCH && self.game.touch))
                         this.broadcastToMafia(Serialize.ChatMessage(self.type, self.index, `<color=#C90000>${self.name}</color>`, message))
+                    else if (self.game.job === JobType.SPIRIT)
+                        this.broadcastToDead(Serialize.ChatMessage(self.type, self.index, `<color=#47C83E>[영매] ${self.name}</color>`, message))
                     else
                         self.send(Serialize.SystemMessage('<color=red>밤에 대화할 수 없습니다.</color>'))
                     break
